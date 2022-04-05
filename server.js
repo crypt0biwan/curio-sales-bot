4afb5b2d2779b20f09b48cbb6f88d3b5280f5c63
crypt0biwan <crypt0biwan@gmail.com>
2022-04-05 14:24:06 +0200

    update msg formatting

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const { WebhookClient } = require('discord.js')
-const ethers = require('ethers')
-const abi = require('./abis/WyvernExchangeWithBulkCancellations.json')
-const { watchForTransfers } = require('./utils/watcher.js')
+const { watchForTransfers } = require('./utils/watcher')
+const { formatMessage } = require('./utils/discord')
 
 require('dotenv').config()
-const { DISCORD_ID, DISCORD_TOKEN, INFURA_PROJECT_ID, INFURA_SECRET } = process.env
+const { DISCORD_ID, DISCORD_TOKEN } = process.env
 const webhookClient = new WebhookClient({id: DISCORD_ID, token: DISCORD_TOKEN});
 
 const postToWebhook = ({qty, card, totalPrice}) => {
-	webhookClient.send(`Found curio sale: ${qty}x CRO${card} sold for ${totalPrice}`).catch(console.error);
+	webhookClient
+		.send(formatMessage({ qty, card, totalPrice }))
+		.catch(console.error);
 };
 
 watchForTransfers(postToWebhook);
--- utils/discord.js
+++ utils/discord.js
@@ -1,13 +1,52 @@
-const Discord = require('discord.js')
 const COLORS = require('./colors')
+const IMAGES = require('./cards')
 
-const formatMessage = ({ title, url, image_url, color = COLORS.LIGHTBLUE, fields }) =>
-	new Discord.MessageEmbed()
-		.setColor(color)
-		.setTitle(title)
-		.setURL(url)
-		.setImage(image_url)
-		.addFields(fields)
+const formatMessage = ({ qty, card, totalPrice }) => {
+	const cardSold = `CRO${card}`
+	const imgHash = IMAGES[cardSold]
+
+	return {
+		username: 'CurioCard Sales',
+		embeds: [
+			{
+				author: {
+					name: 'Curio Cards',
+					// url: 'https://curio.cards',
+					icon_url: 'https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=1,format=auto/https%3A%2F%2F1770801706-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fcollections%252FNyUEXr2B4FBefalwZTqb%252Ficon%252FAbV65JUMTbXuuBMe9I0M%252Favatar2.png%3Falt%3Dmedia%26token%3D99ceb163-5bff-440c-a5b4-83ac5ffc4d1a'
+				},
+				title: `${cardSold} has been sold`,
+				description: `Buyer: **0x0000**
+				Seller: **0x0000**
+				---------------------------------`,
+				url: `https://opensea.io/assets/0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313/${card}`,
+				thumbnail: {
+					url: `https://ipfs.io/ipfs/${imgHash}`
+				},
+				color: COLORS.GREEN,
+				fields: [
+					{
+						name: 'Quantity',
+						value: qty.toString(),
+						inline: true,
+					},
+					{
+						name: 'ETH',
+						value: totalPrice.toFixed(2),
+						inline: true,
+					},
+					{
+						name: 'USD',
+						value: 'xxx',
+						inline: true,
+					}
+				],
+				footer: {
+					text: `Nice Purchase • ${new Date().toLocaleDateString('en-US', {year: 'numeric', month: '2-digit', day: '2-digit'})}`
+				}
+			}
+		]
+	}
+}
 
 module.exports = exports = {
 	formatMessage,
