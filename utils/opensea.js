673796ced4f1f23a8e523aa22e83e7a67d77daec
Kian <fafrd@users.noreply.github.com>
2023-06-17 11:23:21 -0700

    mock OS response

--- utils/opensea.js
+++ utils/opensea.js
@@ -1,30 +1,36 @@
 const axios = require('axios')
 
-const formatETHaddress = address => address.slice(0, 5) + '...' + address.slice(address.length-3, address.length)
+const formatETHaddress = address => address.slice(0, 5) + '...' + address.slice(address.length - 3, address.length)
 
-const getUsername = async address => {
+openSeaClient = async (address) => axios({
+    method: 'get',
+    url: `https://api.opensea.io/user/${address}`,
+    headers: {
+        'X-API-KEY': process.env.OPENSEA_API_KEY
+    }
+})
+
+
+const getUsername = async (os, address) => {
     return new Promise((resolve, reject) => {
-        axios({
-            method: 'get',
-            url: `https://api.opensea.io/user/${address}`,
-            headers: {
-              'X-API-KEY': process.env.OPENSEA_API_KEY
-            }
-          })
-        .then(function (response) {
-            if(response.data.username != null) {
-                resolve(response.data.account.user.username)
-            } else {
+        os(address)
+            .then(function (response) {
+                if (response.data.username != null) {
+                    console.log(response.data.username)
+                    console.log(response.data.account.user.username)
+                    resolve(response.data.account.user.username)
+                } else {
+                    resolve(formatETHaddress(address))
+                }
+            })
+            .catch(function (error) {
+                console.error(error);
                 resolve(formatETHaddress(address))
-            }
-        })
-        .catch(function (error) {
-            console.error(error);
-            resolve(formatETHaddress(address))
-        })
+            })
     })
 }
 
 module.exports = exports = {
-    getUsername
+    getUsername,
+    openSeaClient
 }
