09c9b34510652c9cd59d8e90570ee87fba74cf05
Kian <fafrd@users.noreply.github.com>
2024-12-08 12:24:08 -0800

    Fix tests and bugs after ethers migration

--- utils/watcher.js
+++ utils/watcher.js
@@ -1,35 +1,24 @@
 const Ethers = require("ethers");
 require('dotenv').config()
-const { INFURA_PROJECT_ID, INFURA_SECRET } = process.env
-
-console.log(INFURA_PROJECT_ID, INFURA_SECRET)
-
-// set up provider
-const provider = new Ethers.InfuraProvider(
-	network="mainnet",
-	projectId=INFURA_PROJECT_ID,
-	projectSecret=INFURA_SECRET
-);
+const { RPC_URL } = process.env
+const provider = new Ethers.JsonRpcProvider(RPC_URL);
 
 // contract addresses should be lowercase
-const OPENSEA_CONTRACT = "0x7f268357a8c2552623316e2562d90e642bb538e5";
-const OLD_OPENSEA_CONTRACT = "0x7be8076f4ea4a4ad08075c2508e481d6c946d12b";
-const wyvernAbi = require("../abis/WyvernExchangeWithBulkCancellations.json");
-const wyvernContract = new Ethers.Contract(OPENSEA_CONTRACT, wyvernAbi, provider);
-const erc20TokenAbi = require("../abis/ERC20Token.json");
-
 const OPENSEA_SEAPORT_CONTRACT_1_2 = "0x00000000006c3852cbef3e08e8df289169ede581"
 const OPENSEA_SEAPORT_CONTRACT_1_4 = "0x00000000000001ad428e4906ae43d8f9852d0dd6"
 const OPENSEA_SEAPORT_CONTRACT_1_5 = "0x00000000000000adc04c56bf30ac9d3c0aaf14dc"
 const OPENSEA_SEAPORT_CONTRACT_1_6 = "0x0000000000000068f116a894984e2db1123eb395"
 const seaportAbi = require("../abis/SeaPort.json");
 const seaportContract = new Ethers.Contract(OPENSEA_SEAPORT_CONTRACT_1_6, seaportAbi, provider);
+const erc20TokenAbi = require("../abis/ERC20Token.json");
 
 const CURIO_WRAPPER_CONTRACT = "0x73da73ef3a6982109c4d5bdb0db9dd3e3783f313";
 const CURIO_17B_WRAPPER_CONTRACT = "0x04afa589e2b933f9463c5639f412b183ec062505";
 const curioAbi = require("../abis/CurioERC1155Wrapper.json");
 const curioContract = new Ethers.Contract(CURIO_WRAPPER_CONTRACT, curioAbi, provider);
 const curio17bContract = new Ethers.Contract(CURIO_17B_WRAPPER_CONTRACT, curioAbi, provider);
+const curioEventFilter = curioContract.filters.TransferSingle();
+const curio17bEventFilter = curio17bContract.filters.TransferSingle();
 
 const LOOKSRARE_CONTRACT = "0x59728544b08ab483533076417fbbb2fd0b17ce3a"
 const looksAbi = require("../abis/LooksRare.json");
@@ -42,20 +31,6 @@ const uniswapContract = async () => await new Ethers.Contract(UNISWAP_USDC_ETH_L
 const getEthUsdPrice = async () => await uniswapContract()
 	.then(contract => contract.getReserves())
 	.then(reserves => Number(reserves._reserve0) / Number(reserves._reserve1) * 1e12); // times 10^12 because usdc only has 6 decimals
-
-const curioEventFilter = {
-	address: CURIO_WRAPPER_CONTRACT,
-	topics: [
-		Ethers.id("TransferSingle(address,address,address,uint256,uint256)")
-	]
-};
-
-const curio17bEventFilter = {
-	address: CURIO_17B_WRAPPER_CONTRACT,
-	topics: [
-		Ethers.id("TransferSingle(address,address,address,uint256,uint256)")
-	]
-};
 
 // this is a helper for the unit test
 async function getCurioEventsFromBlock(blockNum) {
@@ -76,9 +51,6 @@ async function handleCurioTransfer(tx) {
 	let totalPrice = 0
 	let token = 'ETH'
 	let platforms = []
-	let wyvernLogRaw = txReceipt.logs.filter(x => {
-		return [OPENSEA_CONTRACT, OLD_OPENSEA_CONTRACT].includes(x.address.toLowerCase())
-	});
 
 	let seaportLogRaw = txReceipt.logs.filter(x => {
 		return [
@@ -97,38 +69,9 @@ async function handleCurioTransfer(tx) {
 	});
 
 	// early return check
-	if (wyvernLogRaw.length === 0 && seaportLogRaw.length === 0 && looksRareLogRaw.length === 0) {
-		console.log("found transfer, but no associated OpenSea (Wyvern or Seaport) or LooksRare sale");
+	if (seaportLogRaw.length === 0 && looksRareLogRaw.length === 0) {
+		console.log("found transfer, but no associated OpenSea (Seaport) or LooksRare sale");
 		return { qty: 0, card: 0, totalPrice: 0};
-	}
-
-	// check for OpenSea (Wyvern contract) sale
-	if(wyvernLogRaw.length) {
-		platforms.push("OpenSea")
-		// Check if related token transfers instead of a regular ETH buy
-		let tokenTransfers = txReceipt.logs.filter(x => {
-			return x.topics.includes(Ethers.keccak256(Ethers.toUtf8Bytes('Transfer(address,address,uint256)')))
-		});
-		// ERC20 token buy
-		let decimals;
-		if (tokenTransfers.length) {
-			const tokenAddress = tokenTransfers[0].address.toLowerCase()
-			const erc20TokenContract = new Ethers.Contract(tokenAddress, erc20TokenAbi, provider);
-
-			const symbol = await erc20TokenContract.symbol()
-			decimals = await erc20TokenContract.decimals()
-			token = symbol
-		}
-		for (let log of wyvernLogRaw) {
-			let wyvernLog = wyvernContract.interface.parseLog(log);
-
-			if(tokenTransfers.length) {
-				totalPrice += parseFloat(Ethers.formatUnits(wyvernLog.args.price.toBigInt(), decimals))
-			} else {
-				// regular ETH buy
-				totalPrice += parseFloat(Ethers.formatEther(wyvernLog.args.price.toBigInt()));
-			}
-		}
 	}
 
 	// check for OpenSea (Seaport contract) sale
@@ -153,8 +96,8 @@ async function handleCurioTransfer(tx) {
 
 			// added a try/catch, the event OrdersMatched apparently failed here
 			try {
-				if(tokenTransfers.length) {
-					totalPrice += parseFloat(Ethers.formatUnits(seaportLog.args.offer[0].amount.toBigInt(), decimals))
+				if (tokenTransfers.length) {
+					totalPrice += parseFloat(Ethers.formatUnits(seaportLog.args.offer[0].amount, decimals))
 				} else {
 					// regular ETH buy
 
@@ -164,11 +107,10 @@ async function handleCurioTransfer(tx) {
 
 					try {
 						// get the transfers of the last argument of the OrderFulfilled method
-						for(let transfer of seaportLog.args[seaportLog.args.length-1]) {
-							totalPrice += parseFloat(Ethers.formatEther(Ethers.BigNumber.from(transfer.amount, 'hex')))
+						for (let transfer of seaportLog.args[seaportLog.args.length-1]) {
+							totalPrice += parseFloat(Ethers.formatEther(transfer.amount, 'hex'))
 						}
 					} catch(e) {
-						// added some logging since the bot crashes no a rare occasion
 						console.log(e)
 						console.log(log)
 					}
@@ -185,7 +127,7 @@ async function handleCurioTransfer(tx) {
 		platforms.push("LooksRare")
 		for (let log of looksRareLogRaw) {
 			let looksLog = looksContract.interface.parseLog(log);
-			totalPrice += parseFloat(Ethers.formatEther(looksLog.args.price.toBigInt()));
+			totalPrice += parseFloat(Ethers.formatEther(looksLog.args.price));
 		}
 		token = 'WETH'
 	}
@@ -208,10 +150,10 @@ async function handleCurioTransfer(tx) {
 	for (let log of curioLogRaw) {
 		curioLog = curioContract.interface.parseLog(log);
 		// which card was transferred?
-		let qty = curioLog.args._value.toNumber();
-		let card = curioLog.args._id.toString();
-		sellers.push(curioLog.args._from.toLowerCase())
-		buyer = curioLog.args._to.toLowerCase()
+		let qty = Number(curioLog.args[4]);
+		let card = Number(curioLog.args[3]);
+		sellers.push(curioLog.args[1].toLowerCase());
+		buyer = curioLog.args[2].toLowerCase();
 		if (!data[card]) {
 			data[card] = 0;
 		}
@@ -223,6 +165,8 @@ async function handleCurioTransfer(tx) {
 	for ( const [card, qty] of Object.entries(data)) {
 		sales.push(`${qty}x CRO${card}`)
 	}
+
+	totalPrice = totalPrice.toFixed(2) // round to 2 decimals
 	console.log(`Found curio sale: ${sales.join(", ")} sold for ${totalPrice} ${token}`)
 	return { data, totalPrice, buyer, seller, ethPrice, token, platforms };
 }
